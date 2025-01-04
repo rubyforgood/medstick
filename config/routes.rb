03cb7bc898f823457f804b34b9f25630b0dc1f5b
amccaugha42 <amymccaughan@gmail.com>
2025-01-04 13:32:49 -0500

    Created provider scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :providers
   get "static/index"
   get "static/page"
   resources :regions
---
+++ spec/factories/providers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :provider do
+    name { "MyString" }
+    provider_type { "MyString" }
+  end
+end
