d544087ed108c8d7ba526f23e61f7a2be64febc0
amccaugha42 <amymccaughan@gmail.com>
2025-01-12 15:45:08 -0500

    WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,16 @@ module ApplicationHelper
   def active_class(path)
     current_page?(path) ? 'active' : ''
   end
+
+  def submit_button(form)
+    form.submit "#{action_name.titleize} #{controller_name.classify.titleize}", class: "btn btn-primary me-1 mb-1"
+  end
+
+  def show_page_icon(item)
+    link_to '<i class="bi bi-search"></i>'.html_safe, item, class: "btn btn-primary btn-sm", aria: { label: "Show #{controller_name.classify.titleize} Details" }
+  end
+
+  def delete_icon(item)
+    link_to '<i class="bi bi-trash"></i>'.html_safe, item, method: :delete, data: { confirm: "Are you sure?" }, class: "btn btn-danger btn-sm"
+  end
 end
