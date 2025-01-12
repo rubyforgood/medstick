af201d155d66fc1081945cdbe13d5265c2b3fa30
amccaugha42 <amymccaughan@gmail.com>
2025-01-12 13:13:22 -0500

    added provier table

--- app/helpers/providers_helper.rb
+++ app/helpers/providers_helper.rb
@@ -1,2 +1,9 @@
 module ProvidersHelper
+  def edit_link(provider)
+    link_to '<i class="bi bi-pencil-square"></i> Edit'.html_safe, edit_provider_path(provider)
+  end
+
+  def destroy_link(provider)
+    button_to "Destroy this provider", @provider, method: :delete, class: "btn btn-danger"
+  end
 end
