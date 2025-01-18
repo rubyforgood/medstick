20279903082d9b621ed7429e67ca716b7acde14b
Danny Collier <294724+dcollie2@users.noreply.github.com>
2025-01-17 20:31:57 -0500

    Create seeds (#41)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,16 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+[ "Global", "Africa", "Oceania", "Asia", "Latin America" ].each do |region_name|
+  Region.find_or_create_by!(name: region_name)
+end
+
+[ [ "sp", "sp_files" ], [ "en", "en_files" ] ].each do |language|
+  Language.find_or_create_by(name: language[0], file_share_folder: language[1])
+end
+
+if Rails.env.development?
+  User.find_or_create_by(email_address: 'admin@test.test', password_digest: BCrypt::Password.create('password'), is_admin: true)
+  User.find_or_create_by(email_address: 'non_admin@test.test', password_digest: BCrypt::Password.create('password'), is_admin: false)
+end
